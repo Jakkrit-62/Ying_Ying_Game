32cb10d6fafc8f7a171e8f5a1953dc22bc0b6d4b
Jakkrit <girock01923@gmail.com>
2023-09-26 01:40:22 +0700

    Read 1 bit

--- send_to_arduino_check3.py
+++ send_to_arduino_check3.py
@@ -1,22 +1,32 @@
 import serial
+import time
+
 
 # กำหนดพอร์ตที่ Arduino ใช้สำหรับ SoftwareSerial ตาม Xpin, Ypin, Zpin
 arduino_port = '/dev/cu.wchusbserial1110'  # แทนด้วยพอร์ตที่ตรงกับการกำหนดใน Arduino
 
 # เริ่มการเชื่อมต่อกับ Arduino ผ่านพอร์ตที่กำหนด
 arduino_serial = serial.Serial(arduino_port, 9600)
+start_time = time.time()
+values=[]
 
 while True:
-    if arduino_serial.inWaiting() >=1 :
-        arduino_serial.flushInput()
-    print("Flush",arduino_serial.inWaiting())
-    arduino_data_btn = (arduino_serial.readline().decode('latin-1').strip())
-    arduino_data_x = int(arduino_serial.readline().decode('latin-1').strip())
-    arduino_data_y = int(arduino_serial.readline().decode('latin-1').strip())
-    #arduino_data_z = int(arduino_serial.readline().decode('latin-1').strip())
-
-    print(f"Received data from Arduino: btn = {arduino_data_btn}, x = {arduino_data_x}, y = {arduino_data_y}")
-    
-
-
-
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+  # เริ่มจับเวลาใหม่สำหรับรอบถัดไป
+    #print("1")
+    #if arduino_serial.inWaiting() >=1 :
+        #arduino_serial.flushInput()
+    #print("Flush",arduino_serial.inWaiting())
+    arduino_data = arduino_serial.read(1).decode()
+    #arduino_data = arduino_data.replace('\r', '2')
+    values.append(arduino_data) # แยกค่าด้วย ',' เพื่อแยกเป็นตัวแปรแต่ละค่า
+    if len(values)==5:
+        print("ARDUINO = ",values)
+        arduino_data_btn_shoot,arduino_data_btn_restart,arduino_data_btn_pause,arduino_data_x, arduino_data_y = map(str, values)
+        print(f"Received data from Arduino0: shoot = {arduino_data_btn_shoot}, restart = {arduino_data_btn_restart}, pause = {arduino_data_btn_pause}")
+        print(f"Received data from Arduino1: x = {arduino_data_x}, y = {arduino_data_y}")
+        values=[]
+        print(f"รอบลูประยะเวลา: {elapsed_time} วินาที")
+        start_time = end_time
+    # #arduino_data_z = int(arduino_serial.readline().decode().strip())
